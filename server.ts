2be372e277c5f9daa7aeb46e5af4a50ad27d1451
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-04-12 23:32:00 +0530

    add config and setup express

--- server.ts
+++ server.ts
@@ -1,1 +1,13 @@
-console.log("hii")
+import app from "./src/app";
+import { config } from "./src/config/config";
+
+const startServer = () =>{
+
+    const port = config.port || 3000;
+
+    app.listen(port, ()=>{
+        console.log(`Listening from port: ${port}`)
+    });
+};
+
+startServer();
