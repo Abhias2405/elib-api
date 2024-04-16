b2684b7b4fbe58e2bddea677714776020d27a071
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-04-16 22:23:00 +0530

    add login endpoint

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -50,6 +50,41 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+    const { email, password } = req.body;
+  
+    // Validate inputs
+    if (!email || !password) {
+      return next(createHttpError(400, "All fields are required"));
+    }
+  
+    try {
+      // Check if user exists
+      const user = await userModel.findOne({ email });
+      if (!user) {
+        return next(createHttpError(404, "User not found."));
+      }
+  
+      // Compare passwords
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return next(createHttpError(400, "Username or password incorrect!"));
+      }
+  
+      // Generate JWT token
+      const token = sign({ sub: user._id }, config.jwtSecret as string, {
+        expiresIn: "7d",
+        algorithm: "HS256",
+      });
+  
+      // Send response
+      res.json({ accessToken: token });
+    } catch (err) {
+      if (err instanceof createHttpError.HttpError) {
+        return next(err);
+      }
+      return next(createHttpError(500, "Internal server error."));
+    }
+  };
 
-
-export { createUser};
+export { createUser, loginUser};
