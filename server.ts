824c8f16add437a679e36be7e18ebf8d58b64ad3
Abhias2405 <iamabhisheksingh56@gmail.com>
2024-04-23 22:46:00 +0530

    minor changes

--- server.ts
+++ server.ts
@@ -2,15 +2,22 @@ import app from "./src/app";
 import { config } from "./src/config/config";
 import connectDB from "./src/config/db";
 
-const startServer = async() =>{
-
+const startServer = async () => {
+  try {
+    // Connect to the database
     await connectDB();
+    console.log("Database connection established.");
 
     const port = config.port || 3000;
 
-    app.listen(port, ()=>{
-        console.log(`Listening from port: ${port}`)
+    // Start the server
+    app.listen(port, () => {
+      console.log(`Server is listening on port: ${port}`);
     });
+  } catch (error) {
+    console.error("Failed to start the server:", error);
+    process.exit(1); 
+  }
 };
 
 startServer();
